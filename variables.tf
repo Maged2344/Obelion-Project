d8ba9e9d5251d00c97691efe9f46d9595cdd5430
Maged Mohamed <62676576+Maged2344@users.noreply.github.com>
2024-11-17 06:36:49 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "aws_region" {
   default = "us-east-1"
 }
-
-variable "key_name" {
+#### for ec2 ssh ####
+variable "key_name" {  
   description = "Name of the AWS key pair"
   default     = "key"
 }
-
+#### for database ####
 variable "db_username" {
   description = "Database username"
 }
